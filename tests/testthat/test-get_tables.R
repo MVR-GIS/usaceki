a1e816a7751d5f4925ec276a60fa57b1e3652839
Michael <mike.p.doc.71@gmail.com>
2025-11-13 09:58:16 -0600

    updated get_tables tests

--- tests/testthat/test-get_tables.R
+++ tests/testthat/test-get_tables.R
@@ -1,5 +1,12 @@
 test_that("get all tables", {
   db_connection = con_roracle
-  exclude_pattern = "."
+  table_vector <- get_tables(db_connection)
+  expect_contains(table_vector, c("PROJECT", "QUESTION"))
+})
+
+test_that("get some tables", {
+  db_connection = con_roracle
+  exclude_pattern = "^(EDW)"
   table_vector <- get_tables(db_connection, exclude_pattern)
+  expect_equal(length(table_vector), 29)
 })
